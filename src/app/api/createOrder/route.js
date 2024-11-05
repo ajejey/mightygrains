f5ed135436a09d4b77575cf7274712caf14e57d2
Ajey <ajejey@gmail.com>
2024-11-05 23:03:33 +0530

    comment out payment code

--- src/app/api/createOrder/route.js
+++ src/app/api/createOrder/route.js
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server";
-import Razorpay from "razorpay";
+// import { NextResponse } from "next/server";
+// import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-    key_id: process.env.NEXT_PUBLIC_RAZORPAY_TEST_KEY_ID,
-    key_secret: process.env.RAZORPAY_TEST_KEY_SECRET,
-});
+// const razorpay = new Razorpay({
+//     key_id: process.env.NEXT_PUBLIC_RAZORPAY_TEST_KEY_ID,
+//     key_secret: process.env.RAZORPAY_TEST_KEY_SECRET,
+// });
 
-export async function POST(request) {
-    const { amount } = await request.json();
-    const order = await razorpay.orders.create({
-        amount,
-        currency: "INR",
-    })
+// export async function POST(request) {
+//     const { amount } = await request.json();
+//     const order = await razorpay.orders.create({
+//         amount,
+//         currency: "INR",
+//     })
 
-    return NextResponse.json(order);
-}
+//     return NextResponse.json(order);
+// }
--- src/app/products/[id]/order/page.js
+++ src/app/products/[id]/order/page.js
@@ -3,51 +3,51 @@ import Script from 'next/script';
 import React, { useState } from 'react'
 
 const OrderPage = () => {
-  const [amount, setAmount] = useState(0);
+  // const [amount, setAmount] = useState(0);
 
-  const createOrder = async () => {
-    const response = await fetch('/api/createOrder', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ amount }),
-    });
-    const data = await response.json();
-    console.log(data);
+  // const createOrder = async () => {
+  //   const response = await fetch('/api/createOrder', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({ amount }),
+  //   });
+  //   const data = await response.json();
+  //   console.log(data);
 
-    const paymentData = {
-      key: process.env.NEXT_PUBLIC_RAZORPAY_TEST_KEY_ID,
-      order_id: data.id,
-      handler: async function (response) {
-        // verify payment
-        const res = await fetch('/api/verifyOrder', {
-          method: 'POST',
-          body: JSON.stringify({
-            orderId: response.razorpay_order_id,
-            razorpayPaymentId: response.razorpay_payment_id,
-            razorpaySignature: response.razorpay_signature
-          })
-        })
-        const data = await res.json();
-        console.log("data", data);
+  //   const paymentData = {
+  //     key: process.env.NEXT_PUBLIC_RAZORPAY_TEST_KEY_ID,
+  //     order_id: data.id,
+  //     handler: async function (response) {
+  //       // verify payment
+  //       const res = await fetch('/api/verifyOrder', {
+  //         method: 'POST',
+  //         body: JSON.stringify({
+  //           orderId: response.razorpay_order_id,
+  //           razorpayPaymentId: response.razorpay_payment_id,
+  //           razorpaySignature: response.razorpay_signature
+  //         })
+  //       })
+  //       const data = await res.json();
+  //       console.log("data", data);
 
-        if (data.isOk) {
-          // do whatever page transition you want as payment is complete
-          alert('Payment verified successfully');
-        } else {
-          alert('Payment verification failed');
-        }
-      }
-    }
+  //       if (data.isOk) {
+  //         // do whatever page transition you want as payment is complete
+  //         alert('Payment verified successfully');
+  //       } else {
+  //         alert('Payment verification failed');
+  //       }
+  //     }
+  //   }
 
-    const payment = new window.Razorpay(paymentData);
-    payment.open();
-  }
+  //   const payment = new window.Razorpay(paymentData);
+  //   payment.open();
+  // }
   
   return (
     <div className='flex flex-col items-center my-12'>
-      <Script type='text/javascript' src="https://checkout.razorpay.com/v1/checkout.js" />
+      {/* <Script type='text/javascript' src="https://checkout.razorpay.com/v1/checkout.js" />
       <h2>Order</h2>
       <input
         type="number"
@@ -61,7 +61,7 @@ const OrderPage = () => {
         onClick={createOrder}
       >
         Create Order
-      </button>
+      </button> */}
     </div>
   )
 }
